007d9c9ab3da050d91f8b09dcdf47e2522301055
Liniker Silva <linikergabrieldasilva@gmail.com>
2022-03-05 20:56:09 -0300

    feat: connecting to mongoDb with repository and service

---
+++ src/main/java/com/cursoudemy/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cursoudemy.workshopmongo.repositories;
+
+import com.cursoudemy.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+
+}
