f66c956ee775f071c238d063150ce1a935c873b6
Liniker Silva <linikergabrieldasilva@gmail.com>
2022-03-07 00:20:09 -0300

    feat: simple query using query methods

--- src/main/java/com/cursoudemy/workshopmongo/resources/PostResource.java
+++ src/main/java/com/cursoudemy/workshopmongo/resources/PostResource.java
@@ -1,24 +1,31 @@
 package com.cursoudemy.workshopmongo.resources;
 
 import com.cursoudemy.workshopmongo.domain.Post;
+import com.cursoudemy.workshopmongo.resources.util.URL;
 import com.cursoudemy.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/posts")
 public class PostResource {
 
     @Autowired
-    private PostService PostService;
+    private PostService postService;
 
     @GetMapping("{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
-        Post obj = PostService.findById(id);
+        Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("titlesearch")
+    public ResponseEntity <List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
