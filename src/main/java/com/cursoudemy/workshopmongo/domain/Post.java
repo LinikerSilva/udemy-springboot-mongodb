f6a6e3189004f2ff59b535f8ab908f15da53c80c
Liniker Silva <linikergabrieldasilva@gmail.com>
2022-03-06 21:15:53 -0300

    feat: author data projection with dto

--- src/main/java/com/cursoudemy/workshopmongo/domain/Post.java
+++ src/main/java/com/cursoudemy/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.cursoudemy.workshopmongo.domain;
 
+import com.cursoudemy.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -14,12 +15,12 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -57,6 +58,14 @@ public class Post implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public AuthorDTO getAuthor() {
+        return author;
+    }
+
+    public void setAuthorDTO(AuthorDTO author) {
+        this.author = author;
     }
 
     @Override
