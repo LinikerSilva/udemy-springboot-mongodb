9387d4f3b3eafca35684f6d7fd39a8073f79dacd
Liniker Silva <linikergabrieldasilva@gmail.com>
2022-03-06 23:55:51 -0300

    feat: getting a post by id

--- src/main/java/com/cursoudemy/workshopmongo/resources/PostResource.java
+++ src/main/java/com/cursoudemy/workshopmongo/resources/PostResource.java
@@ -4,12 +4,10 @@ import com.cursoudemy.workshopmongo.domain.Post;
 import com.cursoudemy.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/posts")
